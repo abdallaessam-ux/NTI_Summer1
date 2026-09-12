11a82eec080b39e55c4737051c5481c90523c1af
abdallaessam-ux <abdallaessam608@gmail.com>
2026-09-12 16:47:41 +0300

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-import numpy as np
-print("its work")
-print ("hello")
-def func(x=25):
-    print (x)
-    
+import streamlit as st
+import pandas as pd
+dic={"age":[10,20,30]}
+code='''
+#include <math.h>
+'''
+
+st.code(code,language="c")
+df=pd.DataFrame(dic)
+st.title("Sales dashboard")
+st.markdown("Revenue is  up 12%  this quarter.")
+st.write({"users": 1280, "growth": 0.12}) # auto-formats
+st.dataframe(df)
+st.metric("Revenue", "$48.2k", "+12%")
+st.line_chart(df)
